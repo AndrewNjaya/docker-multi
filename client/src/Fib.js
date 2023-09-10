42146e47d99864bcaabde39d31344d8587e35909
Andrew Njaya <odhiambonjaya@gmail.com>
2023-09-10 15:55:09 +0300

    [chore] fix client react app

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    let a = []
-    let i = typeof(this.state.seenIndexes) == typeof(a) ? this.state.seenIndexes.map(( number ) => number).join(', ') : this.setState({seenIndexes: a});
-    return i;
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
